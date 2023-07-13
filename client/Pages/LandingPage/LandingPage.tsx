f28a27bbefb041454d88db097ba34457e7f86cba
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-14 11:16:37 +1200

    did some basic set up for the landingPage

--- client/Pages/LandingPage/LandingPage.tsx
+++ client/Pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,17 @@
 function LandingPage() {
   return (
     <>
-      <div>
-        <h1>I am in the Landing Page</h1>
+      <div className="bg-red-200 flex flex-col items-center">
+        <div className="w-32 bg-purple-400  ">
+          <h1>Welcome to the FurEverPage</h1>
+        </div>
+        <div className="w-32 bg-indigo-400">
+          <h1>Slogon</h1>
+        </div>
+        <div>
+          <button className="bg-lime-600">Sign up</button>
+          <button className="bg-yellow-800">Login</button>
+        </div>
       </div>
     </>
   )
