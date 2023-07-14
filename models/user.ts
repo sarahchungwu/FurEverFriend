0c14c6592702c560bd88a4fc12a88e2a132d5c48
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-15 08:39:08 +1200

    created the type for the userData

---
+++ models/user.ts
@@ -0,0 +1,22 @@
+import * as z from 'zod'
+
+export const userDraftSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  pronouns: z.string(),
+  bio: z.string(),
+})
+
+export const userEditSchema = z.object({
+  name: z.string(),
+  pronouns: z.string(),
+  bio: z.string(),
+})
+
+export const usersDataBackendSchema = userDraftSchema.extend({
+  auth0_id: z.string(),
+})
+
+export type UpdateUsersData = z.infer<typeof userEditSchema>
+export type UserData = z.infer<typeof userDraftSchema>
+export type UsersDataBackend = z.infer<typeof usersDataBackendSchema>
