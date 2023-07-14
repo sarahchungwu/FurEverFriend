2b617e44aa06e193ae9d7ec8ac7394b220e78dc0
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-15 07:57:29 +1200

    added stager animation for the words

--- client/Pages/LandingPage/LandingPage.tsx
+++ client/Pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,35 @@
 import LoginButton from '../../components/Login/LoginButton'
 import RegisterButton from '../../components/Register/RegisterButton'
-
+import { motion } from 'framer-motion'
 function LandingPage() {
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-orange-50 min-h-screen p-20 bg-cover bg-cente">
         <div className="mb-20 flex flex-col justify-start pl-6 ">
-          <h1 className="text-2xl text-yellow-900 pb-2">UNITE </h1>
-          <h1 className="text-3xl text-yellow-900 pb-2">IGNITE LIFELONG</h1>
-          <h1 className="text-4xl text-yellow-900">FRIENDSHIPS.</h1>
+          <motion.h1
+            className=" text-2xl text-yellow-900 pb-2"
+            initial={{ opacity: 0, translateY: -50 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            UNITE{' '}
+          </motion.h1>
+          <motion.h1
+            className="text-3xl text-yellow-900 pb-2"
+            initial={{ opacity: 0, translateY: -50 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 1, delay: 1.8 }}
+          >
+            IGNITE LIFELONG
+          </motion.h1>
+          <motion.h1
+            className="text-4xl text-yellow-900"
+            initial={{ opacity: 0, translateY: -50 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 1, delay: 3 }}
+          >
+            FRIENDSHIPS.
+          </motion.h1>
           <img src="../../../public/image/Dog1.png" alt="dogs" />
         </div>
 
