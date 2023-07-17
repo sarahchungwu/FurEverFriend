dd832a0453ae424d1eac190626a72560f3800b5a
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-17 22:07:20 +1200

    changed the icon of the messages

--- client/Pages/HomePage/HomePage.tsx
+++ client/Pages/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { faEnvelope, faMessage } from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useQuery } from 'react-query'
 import { DogsDataBackend } from '../../../models/dog'
@@ -59,14 +59,11 @@ function HomePage() {
             )}
           </div>
         )}
-        <div className="flex flex-col items-center mt-10">
-          {/* <button className="flex gap-10 bg-orange-200 shadow-lg  text-yellow-950 border-none rounded-md px-4 py-2 cursor-pointer hover:bg-orange-300 focus:bg-orange-300 ">
-            View match
-          </button> */}
+        <div className="flex flex-row justify-end mt-10 mr-16">
           <div className="mt-36 ">
             <FontAwesomeIcon
-              icon={faMessage}
-              className="text-4xl text-yellow-900 mb-5"
+              icon={faCommentDots}
+              className="text-5xl text-yellow-900 mb-5"
             />
           </div>
         </div>
