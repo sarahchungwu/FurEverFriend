6b996898c73c6bd2fc34ab8d5405fccf439655a8
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-20 07:29:24 +1200

    create the MatchList Component

--- client/Pages/MatchPage/MatchListPage.tsx
+++ client/Pages/MatchPage/MatchListPage.tsx
@@ -1,18 +1,37 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { faPen, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useQueryClient } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
+import { DogsDataBackend } from '../../../models/dog'
 import { fetchDogsList } from '../../apis/dogs'
+import { fetchMatchList } from '../../apis/matches'
+import MatchList from '../../components/MatchList/MatchList'
 
 function MatchListPage() {
   const { user, getAccessTokenSilently } = useAuth0()
   const queryClient = useQueryClient()
-  // const dogQuery = useQuery({
-  //   queryKey: 'fetchDogsList',
+  const dogListQuery = useQuery({
+    queryKey: 'fetchDogsList',
+    queryFn: async () => {
+      const accessToken = await getAccessTokenSilently()
+      if (user && user.sub) {
+        const response = await fetchDogsList(accessToken)
+
+        return response as DogsDataBackend[]
+      }
+    },
+    enabled: !!user,
+  })
+
+  if (dogListQuery.isLoading) return 'Loading...'
+  console.log('I am the dogList', dogListQuery.data)
+
+  // const matchQuery = useQuery({
+  //   queryKey: 'fetchMatchList',
   //   queryFn: async () => {
   //     const accessToken = await getAccessTokenSilently()
   //     if (user && user.sub) {
-  //       const response = await fetchDogsList(accessToken)
+  //       const response = await fetchMatchList(accessToken, dogId)
 
   //       return response
   //     }
@@ -28,35 +47,7 @@ function MatchListPage() {
 
   return (
     <>
-      <div className="flex flex-row justify-end mr-9 mt-14"></div>
-      {/* {!dogQuery.isLoading &&
-        dogQuery.data &&
-        dogQuery.data.map((dog) => ( */}
-      <div className="profile-container mx-auto max-w-md p-8 text-center flex flex-col items-center mb-5 mt-8 w-10/12 bg-white rounded-lg bg-opacity-70">
-        <h1 className="pt-3 text-3xl  text-yellow-950">Hey, meet the dog</h1>
-        <div className="w-40 h-40 rounded-full mx-auto overflow-hidden shadow-md mt-8 mb-8">
-          <img
-            src="/image/defalutDogImg.png"
-            alt=""
-            className="w-full h-full object-cover"
-          />
-        </div>
-
-        <div className=" text-3xl font-bold mt-3 w-1/3 text-yellow-950">
-          <p className="text-lg">owner: ownername</p>
-        </div>
-        <div className=" text-gray-600 mt-2 mb-8">
-          <p>Age:</p>
-          <p>Breed:</p>
-          <p>Gender:</p>
-          <p>Pesonality:</p>
-        </div>
-        <div className=" p-4 mt-3 rounded-l-md shadow-md w-9/12 bg-orange-200 bg-opacity-50">
-          <div className="h-40 p-2 text-lg text-yellow-950 ">
-            <p>description</p>
-          </div>
-        </div>
-      </div>
+      <MatchList />
     </>
   )
 }
---
+++ client/components/MatchList/MatchList.tsx
@@ -0,0 +1,31 @@
+function MatchList() {
+  return (
+    <div className="profile-container mx-auto max-w-md p-8 text-center flex flex-col items-center mb-5 mt-8 w-10/12 bg-white rounded-lg bg-opacity-70">
+      <h1 className="pt-3 text-3xl  text-yellow-950">Hey, meet the dog</h1>
+      <div className="w-40 h-40 rounded-full mx-auto overflow-hidden shadow-md mt-8 mb-8">
+        <img
+          src="/image/defalutDogImg.png"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div className=" text-3xl font-bold mt-3 w-1/3 text-yellow-950">
+        <p className="text-lg">owner: ownername</p>
+      </div>
+      <div className=" text-gray-600 mt-2 mb-8">
+        <p>Age:</p>
+        <p>Breed:</p>
+        <p>Gender:</p>
+        <p>Pesonality:</p>
+      </div>
+      <div className=" p-4 mt-3 rounded-l-md shadow-md w-9/12 bg-orange-200 bg-opacity-50">
+        <div className="h-40 p-2 text-lg text-yellow-950 ">
+          <p>description</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MatchList
