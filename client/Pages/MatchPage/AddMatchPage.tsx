9ab2f96c31444dfd3a2e58e4df79c6b417dbf757
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-29 17:50:25 +1200

    added some detail for the match dog

--- client/Pages/MatchPage/AddMatchPage.tsx
+++ client/Pages/MatchPage/AddMatchPage.tsx
@@ -76,10 +76,10 @@ function AddMatchPage() {
       <div className="flex flex-row flex-wrap justify-center gap-4 w-10/12">
         {matchedDogs.map((matchedDog) => (
           <div
-            className="w-2/3  text-yellow-950 bg-opacity-30 bg-orange-200 p-4 rounded-lg shadow-md pb-4 mb-14 pt-8 transform transition-transform hover:scale-125"
+            className="w-2/3  text-yellow-950 bg-opacity-30 bg-orange-200 p-4 rounded-lg shadow-md pb-4 mb-14 mt-16 pt-8 transform transition-transform hover:scale-125"
             key={matchedDog.id}
           >
-            <div className="text-center flex flex-col items-center">
+            <div className="text-center flex flex-col items-center ">
               <img
                 src={
                   matchedDog.img
@@ -90,7 +90,19 @@ function AddMatchPage() {
                 className="w-4/5 h- object-cover mx-auto rounded-lg"
               />
 
-              <h2 className="text-l font-normal mt-2 ">{matchedDog.name}</h2>
+              <h2 className="text-ls font-bold mt-2 ">{matchedDog.name}</h2>
+              <h2 className="text-sm font-normal mt-2 ">
+                {matchedDog.age} years old
+              </h2>
+              <h2 className="text-sm font-normal mt-2 ">{matchedDog.breed}</h2>
+              <h2 className="text-sm font-normal mt-2 ">{matchedDog.gender}</h2>
+              <h2 className="text-sm font-normal mt-2 ">
+                I am a {matchedDog.personality} dog
+              </h2>
+              <h2 className="text-sm font-normal mt-2  pt-4">
+                {matchedDog.description}
+              </h2>
+
               <button className="flex gap-10 h-2/3 bg-orange-200 shadow-lg text-yellow-950 border-none rounded-md px-4 py-2 cursor-pointer hover:bg-orange-300 focus:bg-orange-300 mt-28 transform transition-transform hover:scale-150">
                 Accept
               </button>
