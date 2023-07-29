2ce58ffc32ca72a70677ec5673c2c7f3e02825f9
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-29 14:31:39 +1200

    add the userDogQuery to get userDog

--- client/Pages/MatchPage/AddMatchPage.tsx
+++ client/Pages/MatchPage/AddMatchPage.tsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
-import { fetchAllDogs, fetchDogsList } from '../../apis/dogs'
+import { fetchAllDogs, fetchDogById, fetchDogsList } from '../../apis/dogs'
+import { DogsDataBackend } from '../../../models/dog'
 
 function AddMatchPage() {
   const userDogId = Number(useParams().id)
   const { user, getAccessTokenSilently } = useAuth0()
-  const dogQuery = useQuery({
+  const dogsQuery = useQuery({
     queryKey: 'fetchAllDogs',
     queryFn: async () => {
       const accessToken = await getAccessTokenSilently()
@@ -18,11 +19,26 @@ function AddMatchPage() {
     },
     enabled: !!user,
   })
+  //query user Dog (This will use state management to handle)
 
-  console.log(dogQuery.data, 'I am in the DogQuery')
+  console.log(dogsQuery.data, 'I am in the DogQuery')
 
   // logic will be, the userId !== userDogUserId
   // if, else if
+  const userDogQuery = useQuery({
+    queryKey: 'fetchDogById',
+    queryFn: async () => {
+      const accessToken = await getAccessTokenSilently()
+      if (user && user.sub) {
+        const response = await fetchDogById(accessToken, userDogId)
+
+        return response as DogsDataBackend
+      }
+    },
+    enabled: !!user,
+  })
+
+  console.log('I am the userDogQuery', userDogQuery.data)
 
   return (
     <>
