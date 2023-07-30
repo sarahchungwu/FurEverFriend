281c667a18b9a3e80d1b40622af74808910f87ba
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-30 18:11:06 +1200

    added the clean up mock in the test

--- server/routes/users.test.ts
+++ server/routes/users.test.ts
@@ -1,14 +1,17 @@
-import { vi, describe, it, expect } from 'vitest'
+import { vi, describe, it, expect, afterEach } from 'vitest'
 import request from 'supertest'
 import server from '../server'
 import * as db from '../db/users'
 import { getMockToken } from './mockToken'
-import { UsersDataBackend } from '../../models/user'
+import { UserData, UsersDataBackend } from '../../models/user'
 
 vi.mock('../db/users.ts')
 vi.mock('../logger.ts')
 
 describe('GET /api/v1/users', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   it('should return 200 with a user', async () => {
     const fakeUser: UsersDataBackend = {
       auth0Id: '123',
@@ -34,6 +37,57 @@ describe('GET /api/v1/users', () => {
     expect(response.status).toBe(500)
     expect(response.body).toEqual({
       message: 'Unable to get user from the database',
+    })
+  })
+})
+
+describe('POST /api/v1/users', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return 201 when creating a new profile', async () => {
+    const fakeProfile: UserData = {
+      username: 'apple',
+      email: 'banana123@example.com',
+      pronouns: 'she/her',
+      bio: 'She is a apple',
+    }
+
+    vi.mocked(db.upsertProfile).mockResolvedValue()
+    const response = await request(server)
+      .post('/api/v1/users')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProfile)
+    expect(response.status).toBe(201)
+  })
+
+  it('should return 400 if the body does not match the zod schemea', async () => {
+    const fakeProfile = {}
+
+    vi.mocked(db.upsertProfile).mockResolvedValue()
+    const response = await request(server)
+      .post('/api/v1/users')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProfile)
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    const fakeProfile: UserData = {
+      username: 'apple',
+      email: 'banana123@example.com',
+      pronouns: 'she/her',
+      bio: 'She is a apple',
+    }
+
+    vi.mocked(db.upsertProfile).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .post('/api/v1/users')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProfile)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to insert new user to database',
     })
   })
 })
