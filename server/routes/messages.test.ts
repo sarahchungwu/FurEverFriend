6cd2c1ad81f90741247dca88a99c5d39fbde2c99
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-01 12:25:35 +1200

    wrote the test for POST /api/v1/messages and it worked

--- server/routes/messages.test.ts
+++ server/routes/messages.test.ts
@@ -2,7 +2,11 @@ import * as db from '../db/messages'
 import request from 'supertest'
 import server from '../server'
 import { vi, describe, it, expect, afterEach } from 'vitest'
-import { MessageFromBackend } from '../../models/messages'
+import {
+  AddMessage,
+  AddMessageToBackend,
+  MessageFromBackend,
+} from '../../models/messages'
 import { getMockToken } from './mockToken'
 
 vi.mock('../db/messages.ts')
@@ -76,6 +80,61 @@ describe('GET /api/v1/messages/:id', () => {
     expect(response.status).toBe(500)
     expect(response.body).toEqual({
       message: 'Unable to get the message from the database',
+    })
+  })
+})
+
+//POST messages api/v1/messages
+
+describe('POST /api/v1/messages', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('should return 201 when creating a message', async () => {
+    const newFakeMassage: AddMessageToBackend = {
+      receiverId: 'auth0|227',
+      text: 'Nice to meet you',
+      senderId: 'auth0|123',
+      isRead: false,
+      sentAt: new Date().toISOString(),
+    }
+
+    vi.mocked(db.addNewMessage).mockResolvedValue([1])
+    const response = await request(server)
+      .post('/api/v1/messages')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(newFakeMassage)
+    expect(response.status).toBe(201)
+  })
+
+  it('should return 400 if the body does not match the zod schemea', async () => {
+    const fakeProfile = {}
+
+    vi.mocked(db.addNewMessage).mockResolvedValue([1])
+    const response = await request(server)
+      .post('/api/v1/messages')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(fakeProfile)
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 500 when no access token is passed', async () => {
+    const newFakeMassage: AddMessageToBackend = {
+      receiverId: 'auth0|227',
+      text: 'Nice to meet you',
+      senderId: 'auth0|123',
+      isRead: false,
+      sentAt: new Date().toISOString(),
+    }
+
+    vi.mocked(db.addNewMessage).mockRejectedValue(new Error('test'))
+    const response = await request(server)
+      .post('/api/v1/messages')
+      .set('authorization', `Bearer ${getMockToken()}`)
+      .send(newFakeMassage)
+    expect(response.status).toBe(500)
+    expect(response.body).toEqual({
+      message: 'Unable to insert new message to database',
     })
   })
 })
