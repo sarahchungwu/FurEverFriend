687ef974e045b3ea14d8fa3463510609c7960e0a
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-11 11:17:07 +1200

    fixed the some ui styling and remove unused app component

--- client/components/Login/LoginButton.tsx
+++ client/components/Login/LoginButton.tsx
@@ -15,7 +15,7 @@ function LoginButton() {
     // will later use the storty book for button to better work with UI design
     <button
       onClick={handleLogin}
-      className="flex gap-10 shadow-lg bg-orange-200 text-yellow-950 border-none rounded-md px-4 py-2 cursor-pointer hover: hover:bg-orange-300 "
+      className="flex flex-col items-center shadow-lg bg-orange-200 text-yellow-950 border-none rounded-md px-4 py-2 cursor-pointer hover: hover:bg-orange-300 mx-4 w-24"
     >
       Login
     </button>
